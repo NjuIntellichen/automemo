0de69ae8409fd0015c4c61c44175316a4ace1900
ymcfy <709832926@qq.com>
2017-04-02 12:26:22 +0800

    fix

--- src/main/java/com/intellichens/dao/TagDAO.java
+++ src/main/java/com/intellichens/dao/TagDAO.java
@@ -2,6 +2,7 @@ package com.intellichens.dao;
 
 import com.intellichens.model.TagModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -18,6 +19,6 @@ public interface TagDAO extends JpaRepository<TagModel, Integer> {
     List<TagModel> findTagsByRecordId(@Param("rid")Integer rid);
 
     @Query("delete from TagModel tag where tag.recordId=:rid")
-    @Transactional
+    @Modifying
     void deleteByRid(@Param("rid") Integer rid);
 }
